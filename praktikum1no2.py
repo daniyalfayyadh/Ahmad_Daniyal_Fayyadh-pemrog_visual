64730ceef1dee3939155eab526c3f70fca21f6a3
daniyalfayyadh <72639073+daniyalfayyadh@users.noreply.github.com>
2022-03-10 07:17:10 +0700

    Add files via upload

---
+++ praktikum1no2.py
@@ -0,0 +1,12 @@
+print("\n Tabel Perkalian 1-10")
+print(50 * "=")
+for i in range(1,11):
+    for j in range(1,11):
+        x = i*j
+        if x<10:
+            print( " | ", x, end=" | ")
+        elif x >= 10 and x < 100:
+            print(" | ", x, end=" | ")
+        else:
+            print(" | ", x, end=" | ")
+    print()
